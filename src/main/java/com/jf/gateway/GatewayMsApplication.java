ce98cad8887e126dcc71086c2d9851ac934006d8
江峰 <feng.jiang@marketin.cn>
2021-05-26 17:59:41 +0800

    健康检查

--- src/main/java/com/jf/gateway/GatewayMsApplication.java
+++ src/main/java/com/jf/gateway/GatewayMsApplication.java
@@ -3,6 +3,9 @@ package com.jf.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Import;
+
+import com.jf.common.utils.config.JfCommonUtilsConfig;
 
 /**
  * 描述:
@@ -13,6 +16,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@Import({ JfCommonUtilsConfig.class })
 public class GatewayMsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayMsApplication.class, args);
